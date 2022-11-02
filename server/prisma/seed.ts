40a85bbe8e260960eb3c04754100ba75ab11adee
Caio Rezador <caio.rezador@gmail.com>
2022-11-02 00:37:47 -0300

    Codigo realizado nas aulas 1 e 2

---
+++ server/prisma/seed.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data:{
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      avatarURL: 'https://github.com/Rcay.png'
+    }
+  });
+
+  const pool = await prisma.pool.create({
+    data:{
+      title: 'Example Pool',
+      code: 'BOL123',
+      ownerId: user.id,
+
+      participants: {
+        create:{
+          userId: user.id
+        }
+      }
+    }
+  });
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-02T12:00:00.907Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    }
+  });
+
+  await prisma.game.create({
+    data:{
+      date: '2022-11-04T00:46:06.907Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses:{
+        create: {
+          firtsTeamPoints: 2,
+          secondteamPoints: 1,
+
+          participant:{
+            connect:{
+              userId_poolId:{
+                userId: user.id,
+                poolId: pool.id
+              }
+            }
+          }
+        }
+      }
+    },
+  });
+
+}
+
+main();
